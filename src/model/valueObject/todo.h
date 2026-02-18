5a41cdf455b0cb8ec0725b82cb19928f1bd171af
Saad <saadmuizz@gmail.com>
2026-02-18 06:58:09 -0500

    update

--- src/model/valueObject/todo.h
+++ src/model/valueObject/todo.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <stdbool.h>
-#include <stddef.h>
 #include <stdint.h>
 
 #define MAX_TODOS 10
@@ -21,15 +20,3 @@ struct Todo {
 };
 
 void todo_print(const struct Todo todos[], const char *type);
-
-bool todo_parseObject(struct Todo *out, const char *object);
-
-size_t todo_parseArray(struct Todo *out, size_t max, const char *json);
-
-size_t todo_parsePointers(struct Todo **out, const char *json);
-
-bool todo_stringifyObject(const struct Todo *todo, char *out, size_t buffer);
-
-size_t todo_stringifyArray(const struct Todo *todos, size_t count, char *buffer, size_t size);
-
-size_t todo_stringifyPointers(struct Todo **todos, char *out, size_t size);
